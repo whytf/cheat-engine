5adbc7081d6a3a4928cc66997d121e948c510c2a
whytf <lukasko.g@gmail.com>
2024-01-11 18:48:11 +0100

    obfs

--- Cheat Engine/dbk32/DBK32functions.pas
+++ Cheat Engine/dbk32/DBK32functions.pas
@@ -3191,19 +3191,19 @@ begin
         if not fileexists(dataloc) then
         begin
 
-          servicename:='CEDRIVER73';
+          servicename:='nezbednyjazdec';
           ultimapservicename:='ULTIMAP2';
           processeventname:='DBKProcList60';
           threadeventname:='DBKThreadList60';
 
           if iswow64 then
           begin
-            sysfile:='dbk64.sys';
+            sysfile:='nezbednysubor64.sys';
             ultimapsysfile:='ultimap2-64.sys';
           end
           else
           begin
-            sysfile:='dbk32.sys';
+            sysfile:='nezbednysubor32.sys';
             ultimapsysfile:='';
           end;
 
